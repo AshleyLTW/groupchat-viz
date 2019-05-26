2d0f4cb1a7b634f3402167e31cb1c8ada6f2bab2
Ashley Wong <ashleyltwong@gmail.com>
2019-05-26 21:46:15 +1000

    json compatible with new framework

--- parse.py
+++ parse.py
@@ -1,8 +1,10 @@
 import csv
 import json
+from collections import Counter
 
 names = []
 links = []
+weighted_links = []
 
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter =',')
@@ -11,7 +13,7 @@ with open('data.csv') as csv_file:
         if ctr == 0:
             index = 0
             for col in row:
-                names.append({"id": index, "name": col})
+                names.append({"nodeName": col, "group": index})
                 index += 1
             ctr += 1
         else:
@@ -31,7 +33,8 @@ with open('data.csv') as csv_file:
                             if row[target_index] == "0":
                                 pass
                             elif row[target_index] == "1":
-                                links.append({"source": source_index, "target": target_index})
+                                # links.append({"source": source_index, "target": target_index})
+                                links.append((source_index, target_index))
                             else:
                                 print("bad target data")
                             target_index += 1
@@ -39,7 +42,15 @@ with open('data.csv') as csv_file:
                         print("this data is not clean" + src_col)
             ctr += 1
 
-jObject = {"nodes": names, "links": links}
+# Weight links
+set_links = set(links)
+link_frequency = Counter(links)
+for link in set_links:
+    freq = link_frequency[link]
+    weighted_links.append({"source": link[0], "target": link[1], "value": freq})
+
+
+jObject = {"nodes": names, "links": weighted_links}
 obj = print(json.dumps(jObject))
 
 
